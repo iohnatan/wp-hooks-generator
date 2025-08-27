1e3271f0698c1064450776148cf1eaef4c61700d
Jonathan S <37553600+iohnatan@users.noreply.github.com>
2025-08-27 18:59:23 -0400

    pull (#1)
    * do not exit if docblock doesnt exist for a hook
    
    * Version 1.0.1.
    
    ---------
    
    Co-authored-by: pramodjodhani <mrpramodjodhani@gmail.com>
    Co-authored-by: John Blackbourn <john@johnblackbourn.com>

--- src/generate.php
+++ src/generate.php
@@ -275,17 +275,13 @@ function hooks_parse_files( array $files, string $root, array $ignore_hooks ) : 
 			];
 
 			if ( ! ( $docblock instanceof Doc ) ) {
-				if ( in_array( $hook_name, $known_problem_hooks, true ) ) {
-					continue;
-				}
-
-				throw new \Exception(
-					sprintf(
-						'Hook "%s" in file "%s" is missing a docblock.',
-						$hook_name,
-						$filename,
-					)
+				echo sprintf(
+					"Hook '%s' in file '%s' is missing a docblock.\n",
+					$hook_name,
+					$filename,
 				);
+
+				continue;
 			}
 
 			$dbt = $docblock ? $docblock->getText() : '';
@@ -295,13 +291,13 @@ function hooks_parse_files( array $files, string $root, array $ignore_hooks ) : 
 					continue;
 				}
 
-				throw new \Exception(
-					sprintf(
-						'Hook "%s" in file "%s" has an empty docblock.',
-						$hook_name,
-						$filename,
-					)
+				echo sprintf(
+					"Hook '%s' in file '%s' has an empty docblock.\n",
+					$hook_name,
+					$filename,
 				);
+
+				continue;
 			}
 
 			$doc = [
@@ -493,7 +489,7 @@ $actions = array_values( array_filter( $output, function( array $hook ) : bool {
 } ) );
 
 $actions = [
-	'$schema' => 'https://raw.githubusercontent.com/wp-hooks/generator/1.0.0/schema.json',
+	'$schema' => 'https://raw.githubusercontent.com/wp-hooks/generator/1.0.1/schema.json',
 	'hooks' => $actions,
 ];
 
@@ -505,7 +501,7 @@ $filters = array_values( array_filter( $output, function( array $hook ) : bool {
 } ) );
 
 $filters = [
-	'$schema' => 'https://raw.githubusercontent.com/wp-hooks/generator/1.0.0/schema.json',
+	'$schema' => 'https://raw.githubusercontent.com/wp-hooks/generator/1.0.1/schema.json',
 	'hooks' => $filters,
 ];
 
